d45e3a3f44dc3a55de471d9bfbbe2ee64e740cb8
elisahonorato <69485908+elisahonorato@users.noreply.github.com>
2021-07-23 16:42:33 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 let labels0 = ["Horas", "Animo", "Rendimiento", "Clima", "Deporte", "Sueño"];
 
-let labels1 = ["10/04", "12/04", "16/04", "30/04","10/05", "10/04", "12/04", "16/04", "30/04", "10/05","10/05", "10/04", "12/04", "16/04", "30/04", "01/07"];
-let labels2 = ["10/04 imput", "10/04 output", "12/04 imput", "12/04 output", "16/04 imput", "16/04 output", "27/04 imput", "06/05 output", "10/05 imput", "10/05 output", "10/04 imput", "10/04 output", "12/04 imput", "12/04 output", "16/04 imput", "16/04 output", "27/04 imput", "06/05 output", "10/05 imput", "10/05 output","10/05 imput", "10/05 output", "10/04 imput", "10/04 output", "12/04 imput", "12/04 output", "16/04 imput", "16/04 output", "27/04 imput", "06/05 output", "01/07 imput", "01/07 output","10/05 imput", "10/05 output", "10/04 imput", "10/04 output", "12/04 imput", "12/04 output", "16/04 imput", "16/04 output", "27/04 imput", "06/05 output", "10/05 imput", "10/05 output","10/05 imput", "10/05 output", "10/04 imput", "10/04 output", "12/04 imput", "12/04 output", "16/04 imput", "16/04 output", "27/04 imput", "06/05 output", "01/07 imput", "01/07 output"];
+let labels1 = ["10/04", "12/04", "16/04", "30/04", "10/05", "10/04", "12/04", "16/04", "30/04", "10/05", "10/05", "10/04", "12/04", "16/04", "30/04", "01/07"];
+let labels2 = ["10/04 imput", "10/04 output", "12/04 imput", "12/04 output", "16/04 imput", "16/04 output", "27/04 imput", "06/05 output", "10/05 imput", "10/05 output", "10/04 imput", "10/04 output", "12/04 imput", "12/04 output", "16/04 imput", "16/04 output", "27/04 imput", "06/05 output", "10/05 imput", "10/05 output", "10/05 imput", "10/05 output", "10/04 imput", "10/04 output", "12/04 imput", "12/04 output", "16/04 imput", "16/04 output", "27/04 imput", "06/05 output", "01/07 imput", "01/07 output"];
 
-let horas1 = [10, 6, 2, 16, 4,10, 6, 2, 16, 4,4,10, 6, 2, 16, 4];
-let horas2 = [10, 10, 6, 6, 2, 2, 5, 10, 2, 2,10, 10, 6, 6, 2, 2, 5, 10, 2, 2, 2, 2,10, 10, 6, 6, 2, 2, 5, 10, 2, 2];
+let horas1 = [10, 6, 2, 16, 4, 10, 6, 2, 16, 4, 4, 10, 6, 2, 16, 4];
+let horas2 = [10, 10, 6, 6, 2, 2, 5, 10, 2, 2, 10, 10, 6, 6, 2, 2, 5, 10, 2, 2, 2, 2, 10, 10, 6, 6, 2, 2, 5, 10, 2, 2];
 
-let animos1 = [6, 3.5, 2.5, 5, 8,6, 3.5, 2.5, 5, 8, 8,6, 3.5, 2.5, 5, 8];
-let animos2 = [8, 4, 7, 2, 5, 0, 10, 4, 7, 9,8, 4, 7, 2, 5, 0, 10, 4, 7, 9,7, 9,8, 4, 7, 2, 5, 0, 10, 4, 7, 9];
+let animos1 = [6, 3.5, 2.5, 5, 8, 6, 3.5, 2.5, 5, 8, 8, 6, 3.5, 2.5, 5, 8];
+let animos2 = [8, 4, 7, 2, 5, 0, 10, 4, 7, 9, 8, 4, 7, 2, 5, 0, 10, 4, 7, 9, 7, 9, 8, 4, 7, 2, 5, 0, 10, 4, 7, 9];
 
-let rendimientos1 = [6, 10, 1, 4, 9,6, 10, 1, 4, 9, 9,6, 10, 1, 4, 9];
-let rendimientos2 = [0, 6, 7, 3, 0, 0, 2, 6, 7, 2,0, 6, 7, 3, 0, 0, 2, 6, 7, 2, 7, 2,0, 6, 7, 3, 0, 0, 2, 6, 7, 2]
+let rendimientos1 = [6, 10, 1, 4, 9, 6, 10, 1, 4, 9, 9, 6, 10, 1, 4, 9];
+let rendimientos2 = [0, 6, 7, 3, 0, 0, 2, 6, 7, 2, 0, 6, 7, 3, 0, 0, 2, 6, 7, 2, 7, 2, 0, 6, 7, 3, 3, 6, 2, 8, 7, 2]
 
-let climas1 = [7, 5, 4, 3, 4,7, 5, 4, 3, 4, 4,7, 5, 4, 3, 4];
-let climas2 = [7, 7, 5, 5, 4, 4, 4, 0, 4, 4,7, 7, 5, 5, 4, 4, 4, 0, 4, 4, 4, 4,7, 7, 5, 5, 4, 4, 4, 0, 4, 4];
+let climas1 = [7, 5, 4, 3, 4, 7, 5, 4, 3, 4, 4, 7, 5, 4, 3, 4];
+let climas2 = [7, 7, 5, 5, 4, 4, 4, 0, 4, 4, 7, 7, 5, 5, 4, 4, 4, 0, 4, 4, 4, 4, 7, 7, 5, 5, 4, 4, 4, 0, 4, 4];
 
-let deportes1 = [0, 4, 0, 7, 8,0, 4, 0, 7, 8, 8,0, 4, 0, 7, 8];
-let deportes2 = [0, 0, 4, 4, 0, 0, 9, 5, 10, 6,0, 0, 4, 4, 0, 0, 9, 5, 10, 6, 10, 6,0, 0, 4, 4, 0, 0, 9, 5, 10, 6];
+let deportes1 = [0, 4, 0, 7, 8, 0, 4, 0, 7, 8, 8, 0, 4, 0, 7, 8];
+let deportes2 = [0, 0, 4, 4, 0, 0, 9, 5, 10, 6, 0, 0, 4, 4, 0, 0, 9, 5, 10, 6, 10, 6, 0, 0, 4, 4, 0, 0, 9, 5, 10, 6];
 
-let suenos1 = [8, 7, 5, 5, 6,8, 7, 5, 5, 6, 6,8, 7, 5, 5, 6];
-let suenos2 = [8, 8, 8, 8, 5, 5, 3, 7, 6, 6,8, 8, 8, 8, 5, 5, 3, 7, 6, 6, 7, 6, 6,8, 8, 8, 8, 5, 5, 3, 7, 6, 6];
+let suenos1 = [8, 7, 5, 5, 6, 8, 7, 5, 5, 6, 6, 8, 7, 5, 5, 6];
+let suenos2 = [8, 8, 8, 8, 5, 5, 3, 7, 6, 6, 8, 8, 8, 8, 5, 5, 3, 7, 6, 6, 7, 6, 6, 8, 8, 8, 8, 5, 5, 3, 7, 6, 6];
 
-let colors1 = ['#ffdc5e', '#ffbf81', '#ffa3a5', '#ff86c8', '#ff69eb','#da1884', '#c22a8f', '#aa3b9b', '#825aaf', '#6170bd', '#16ebe4','#52ffb8', '#007f5f', '#2b9348', '#80b918', '#aacc00'];
+let colors1 = ['#ffdc5e', '#ffbf81', '#ffa3a5', '#ff86c8', '#ff69eb', '#da1884', '#c22a8f', '#aa3b9b', '#825aaf', '#6170bd', '#16ebe4', '#52ffb8', '#007f5f', '#2b9348', '#80b918', '#aacc00'];
 
 var colorneutro = ['#000000'];
 var colorneutro2 = ['#606060'];
@@ -101,26 +101,26 @@ let chart4 = new Chart(myChart4, {
     data: {
         labels: labels2,
         datasets: [{
-            data: rendimientos2,
+            label: labels0[2],
             fill: false,
+            data: rendimientos2,
             backgroundColor: colorneutro,
-            borderColor: colors1[0],
+            borderColor: colors1[5],
             pointRadius: 1,
             borderWidth: 4,
             lineTension: 0.4,
 
-        }]
+        }, ]
     },
     options: {
         title: {
-            text: "Rendimiento al inicio y al final (escala del 1 al 10)",
+            text: "Rendimiento y Animo",
             display: true
-        },
-        legend: {
-            display: false
         }
     }
+
 });
+
 let myChart5 = document.getElementById("myChart5").getContext('2d');
 
 let chart5 = new Chart(myChart5, {
@@ -128,21 +128,44 @@ let chart5 = new Chart(myChart5, {
     data: {
         labels: labels1,
         datasets: [{
-            data: animos1,
-            backgroundColor: colors1
+                label: labels0[2],
+                fill: true,
+                data: rendimientos1,
+                backgroundColor: colors1[5],
+                pointRadius: 1,
+                borderWidth: 4,
+                lineTension: 0.4,
+            },
+            {
+                label: labels0[1],
+                fill: false,
+                data: animos1,
+                backgroundColor: colors1[2],
+                pointRadius: 1,
+                borderWidth: 4,
+                lineTension: 0.4,
+            },
+            {
+                label: labels0[5],
+                fill: false,
+                data: suenos1,
+                backgroundColor: colors1[8],
 
-        }]
+                pointRadius: 1,
+                borderWidth: 4,
+                lineTension: 0.4,
+            }
+        ]
     },
     options: {
         title: {
-            text: "Estado de Animo (escala del 1 al 10)",
+            text: "Rendimiento y Animo",
             display: true
-        },
-        legend: {
-            display: false
         }
     }
+
 });
+
 
 let myChart6 = document.getElementById("myChart6").getContext('2d');
 
@@ -155,7 +178,7 @@ let chart6 = new Chart(myChart6, {
                 fill: false,
                 data: rendimientos1,
                 backgroundColor: colorneutro,
-                borderColor: colors1[0],
+                borderColor: colors1[5],
                 pointRadius: 1,
                 borderWidth: 4,
                 lineTension: 0.4,
@@ -167,12 +190,10 @@ let chart6 = new Chart(myChart6, {
                 fill: false,
                 data: climas1,
                 backgroundColor: colorneutro,
-                borderColor: colors1[1],
+                borderColor: colors1[11],
                 pointRadius: 1,
                 borderWidth: 4,
                 lineTension: 0.4,
-
-
             }
         ]
     },
@@ -195,7 +216,7 @@ let chart10 = new Chart(myChart10, {
                 fill: false,
                 data: rendimientos1,
                 backgroundColor: colorneutro,
-                borderColor: colors1[3],
+                borderColor: colors1[5],
                 pointRadius: 1,
                 borderWidth: 4,
                 lineTension: 0.4,
@@ -206,7 +227,17 @@ let chart10 = new Chart(myChart10, {
                 fill: false,
                 data: climas1,
                 backgroundColor: colorneutro,
-                borderColor: colors1[9],
+                borderColor: colors1[11],
+                pointRadius: 1,
+                borderWidth: 4,
+                lineTension: 0.4,
+            },
+            {
+                label: labels0[1],
+                fill: false,
+                data: animos1,
+                backgroundColor: colorneutro,
+                borderColor: colors1[2],
                 pointRadius: 1,
                 borderWidth: 4,
                 lineTension: 0.4,
@@ -216,7 +247,7 @@ let chart10 = new Chart(myChart10, {
                 fill: false,
                 data: horas1,
                 backgroundColor: colorneutro,
-                borderColor: colors1[2],
+                borderColor: colors1[9],
                 pointRadius: 1,
                 borderWidth: 4,
                 lineTension: 0.4,
@@ -227,7 +258,7 @@ let chart10 = new Chart(myChart10, {
                 fill: false,
                 data: deportes1,
                 backgroundColor: colorneutro,
-                borderColor: colors1[7],
+                borderColor: colors1[14],
                 pointRadius: 1,
                 borderWidth: 4,
                 lineTension: 0.4,
@@ -237,7 +268,7 @@ let chart10 = new Chart(myChart10, {
                 fill: false,
                 data: suenos1,
                 backgroundColor: colorneutro,
-                borderColor: colors1[4],
+                borderColor: colors1[8],
                 pointRadius: 1,
                 borderWidth: 4,
                 lineTension: 0.4,
